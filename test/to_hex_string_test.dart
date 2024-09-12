a929537cd8b5f74174ed8a6c428d909acda9d255
pixel <pixel+github@chrissx.de>
2024-09-12 11:31:56 +0200

    1.0.1

--- test/to_hex_string_test.dart
+++ test/to_hex_string_test.dart
@@ -1,19 +1,17 @@
 import 'package:to_hex_string/to_hex_string.dart';
-import 'package:test/test.dart';
+import 'package:tested/tested.dart';
 
 void main() {
-  group(
-      'unpadded',
-      () => <int, String>{
-            -1: 'ffffffffffffffff',
-            16: '10',
-          }.forEach((k, v) =>
-              test('$k', () => expect(v, k.toHexString(pad: false)))));
-  group(
-      'padded',
-      () => <int, String>{
-            -1: 'ffffffffffffffff',
-            16: '0000000000000010',
-          }.forEach(
-              (k, v) => test('$k', () => expect(v, k.toHexString(pad: true)))));
+  tests(
+      [
+        (-1, 'ffffffffffffffff'),
+        (16, '10'),
+      ].map((x) => expectTestCase(() => x.$1.toHexString(), x.$2)),
+      'unpadded');
+  tests(
+      [
+        (-1, 'ffffffffffffffff'),
+        (16, '0000000000000010'),
+      ].map((x) => expectTestCase(() => x.$1.toHexString(pad: true), x.$2)),
+      'padded');
 }
